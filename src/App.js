d6e6667ce1b9503fa22802a7d5ee2331a44c6076
Rizwan Ahmad <codeforrizwan@gmail.com>
2022-05-20 11:10:15 -0700

    Created the header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import Header from "./components/Header";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container">
+      <Header title="Hello" />
+    </div>
+  );
 }
 
 export default App;
