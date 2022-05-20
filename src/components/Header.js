d6fae7206d2b84623251f71e4db37654dc7f332b
Rizwan Ahmad <codeforrizwan@gmail.com>
2022-05-20 11:24:08 -0700

    Created the button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,10 @@
+import Button from "./Button";
+
 const Header = ({ title }) => {
   return (
-    <header>
-      <h1 style={}>{title}</h1>
+    <header className="header">
+      <h1>{title}</h1>
+      <Button color="black" text="Hello" />
     </header>
   );
 };
